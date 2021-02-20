8c93d384294b5b6627ecaabbd64649d7e49275df
HirokiItabashi <itaboo.1014@gmail.com>
2021-02-20 23:17:41 +0900

    変更しました

--- scripts/app-2.js
+++ scripts/app-2.js
@@ -2,7 +2,10 @@ var app2 = new Vue({
     el: '#app-2',
     data: {
         slides: [
-            { image: '', text: '' },
+            { image: 'images/whois.jpg', text: 'who is hiroki itabashi' },
+            { image: 'images/ssw.jpg', text: 'he is ssw' },
+            { image: 'images/rec-engineer', text: 'he is enginner' },
+            { image: 'images/ssw.jpg', text: 'he is ssw' },
         ]
     }
 }) 
