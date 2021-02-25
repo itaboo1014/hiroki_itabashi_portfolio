393b28e07e756a5bd6350d4ea17412f86ae4af1b
HirokiItabashi <itaboo.1014@gmail.com>
2021-02-25 20:00:42 +0900

    202102252000

--- scripts/app-3.js
+++ scripts/app-3.js
@@ -2,7 +2,15 @@ var app3 = new Vue({
     el: '#app-3',
     data: {
         works: [
-            { link: '', image: '', title: '', text: '' },
+            { link: '', image: 'images/hwg.jpg', title: 'Hand Wired Garage オフィシャルサイト', text: 'プランニング/デザイン/コーディング' },
+            { link: '', image: 'images/honesty.jpg', title: '「Honesty」いたばしひろき', text: '歌/レコーディング/撮影' },
+            { link: '', image: 'images/the_answer_is_you.jpg', title: '「The Answer Is You ~Re-Recorded~」しゅんひろ', text: 'レコーディング' },
+            { link: '', image: 'images/open_your_path.jpg', title: '「open your path」しゅんひろ', text: 'レコーディング/アレンジ' },
+            { link: '', image: 'images/shitouto.jpg', title: '「しとうと」櫻あつみ', text: 'レコーディング/アレンジ/編曲' },
+            { link: '', image: 'images/tashikanamono.jpg', title: '「確かなもの」いたばしひろき', text: '作詞・作曲/レコーディング/撮影' },
+            { link: '', image: 'images/wakamonoyo.jpg', title: '「若者よ。」いたばしひろき', text: '作詞・作曲/レコーディング/撮影' },
+            { link: '', image: 'images/itabashihiroki_1.jpg', title: '「いたばしひろき1」いたばしひろき', text: '作詞・作曲/レコーディング/デザイン' },
+            { link: '', image: 'images/gogatunoomoide.jpg', title: '「五月の思い出」いたばしひろき', text: '作詞・作曲/レコーディング/撮影' },
         ]
     }
 });
