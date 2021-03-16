a68b2266b8897cc6d1e42fa9b768276e093f80d9
HirokiItabashi <itaboo.1014@gmail.com>
2021-03-17 00:26:38 +0900

    ブログコンテンツを更新しました。

--- scripts/app-5.js
+++ scripts/app-5.js
@@ -2,9 +2,9 @@ var app5 = new Vue({
     el: '#app-5',
     data: {
         blogs: [
-            { link: 'pages/blog_20210313.html', title: 'ブログタイトル1', text: 'comming soon...', image: 'images/ssw.jpg' },
-            { link: '', title: 'ブログタイトル2', text: 'comming soon...', image: 'images/whois.JPG' },
-            { link: '', title: 'ブログタイトル3', text: 'comming soon...', image: 'images/rec-engineer.jpg' }
+            { link: 'pages/blog_20201203.html', title: '「自分軸で生きていく」', text: '2020/12/03', image: '' },
+            { link: 'pages/blog_20201008.html', title: '「若者よ。の制作秘話」', text: '2020/10/08', image: '' },
+            { link: 'pages/blog_20200901.html', title: '「承認欲求から生まれる音楽」', text: '2020/09/01', image: '' }
         ]
     }
 });
