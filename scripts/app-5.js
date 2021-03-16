41bfe8094a056afc9e5c717a3bf3a4fa5a1f3929
itaboo1014 <itabo.1014@gmail.com>
2021-03-16 21:53:26 +0900

    ブログのテンプレートを作成しました。

--- scripts/app-5.js
+++ scripts/app-5.js
@@ -2,9 +2,9 @@ var app5 = new Vue({
     el: '#app-5',
     data: {
         blogs: [
-            { title: 'ブログタイトル1', text: 'comming soon...', image: 'images/ssw.jpg' },
-            { title: 'ブログタイトル2', text: 'comming soon...', image: 'images/whois.JPG' },
-            { title: 'ブログタイトル3', text: 'comming soon...', image: 'images/rec-engineer.jpg' }
+            { link: 'pages/blog_20210313.html', title: 'ブログタイトル1', text: 'comming soon...', image: 'images/ssw.jpg' },
+            { link: '', title: 'ブログタイトル2', text: 'comming soon...', image: 'images/whois.JPG' },
+            { link: '', title: 'ブログタイトル3', text: 'comming soon...', image: 'images/rec-engineer.jpg' }
         ]
     }
 });
